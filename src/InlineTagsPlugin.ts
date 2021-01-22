022796cd404c21465918890a9c398d784293fa2b
Roman <musinrr@gmail.com>
2021-01-22 22:54:33 +0000

    Initial commit

---
+++ src/InlineTagsPlugin.ts
@@ -0,0 +1,83 @@
+module.exports = {
+	default: function(context: any) {
+
+		const buildHints = async (prefix: string) =>{
+			const tags = await context.postMessage({
+				command: 'getTags',
+			});
+			let hints = [];
+			if (prefix !== '#') {
+				hints.push({
+					text: `(+) Create new tag: ${prefix}`,
+					hint: (cm, data, completion) => {
+						// TODO create and set tag
+						cm.replaceRange(prefix, completion.from || data.from, completion.to || data.to, "complete");
+					}
+				});
+			}
+			for (let i = 0; i < tags.length; i++) {
+				const tag = tags[i];
+				if (prefix === '#' || tag.title.startsWith(prefix)) {
+					hints.push({
+						text: '#' + tag.title,
+						displayText: tag.title,
+						hint: async (cm, data, completion) => {
+							context.postMessage({
+								command: 'setTag',
+								tag: tag,
+							});
+							cm.replaceRange(tag.title, completion.from || data.from, completion.to || data.to, "complete");
+						}
+					});
+				}
+			}
+			return hints;
+		}
+
+		const plugin = function(CodeMirror) {
+			CodeMirror.defineOption('inlineTags', false, function(cm, value, prev) {
+				if (!value) return;
+
+				cm.on('inputRead', async function (cm1, change) {
+					if (!cm1.state.completionActive && change.text[0] === '#') {
+						const hint = function(cm, callback) {
+							const cursor = cm.getCursor();
+							const token = cm.getTokenAt(cursor);
+
+							buildHints(token.string).then(hints => {
+								callback({
+									list: hints,
+									from: {line: change.from.line, ch: change.from.ch + 1},
+									to: {line: change.to.line, ch: change.to.ch + 1},
+								});
+							});
+						};
+
+						setTimeout(function () {
+							CodeMirror.showHint(cm, hint, {
+								completeSingle: false,
+								closeOnUnfocus: true,
+								async: true,
+							});
+						}, 10);
+					}
+				});
+			});
+		};
+
+		return {
+			plugin: plugin,
+			codeMirrorResources: [
+			    'addon/hint/show-hint',
+			    ],
+			codeMirrorOptions: {
+    			'inlineTags': true, //TODO a better way?
+			},
+			assets: function() {
+			    return [
+			        { name: './show-hint.css'},
+			    ]
+			}
+        }
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import joplin from 'api';
+import JoplinData from 'api/JoplinData';
+import { ContentScriptType, Path } from 'api/types';
+
+async function getAll(api: JoplinData, path: Path, query: any): Promise<any[]> {
+	query.page = 1;
+	let response = await api.get(path, query);
+	let result = !!response.items ?  response.items : [];
+	while (!!response.has_more) {
+		query.page += 1;
+		let response = await api.get(path, query);
+		result.concat(response.items)
+	}
+	return result;
+}
+
+joplin.plugins.register({
+	onStart: async function() {
+		await joplin.contentScripts.register(
+			ContentScriptType.CodeMirrorPlugin,
+			'inlineTags',
+			'./InlineTagsPlugin.js'
+		);
+
+		await joplin.contentScripts.onMessage('inlineTags', async (message: any) => {
+			const selectedNoteIds = await joplin.workspace.selectedNoteIds();
+			const noteId = selectedNoteIds[0];
+
+			if (message.command === 'getTags') {
+				let allTags = await getAll(joplin.data, ['tags'], { fields: ['id', 'title'], page: 1 });
+				const noteTags = await getAll(joplin.data, ['notes', noteId, 'tags'], { fields: ['id'], page: 1 });
+				allTags = allTags.filter(t => !noteTags.includes(t.id));
+				return allTags;
+			} else if (message.command === 'setTag') {
+				await joplin.data.post(['tags', message.tag.id, 'notes'], null, {
+					id: noteId
+				});
+			}
+		});
+	},
+});
